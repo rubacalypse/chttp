128d2f61a131c075e55d6fc4a1e328af7de3c52e
Ruba Merza <ruba.merza@gmail.com>
2015-07-13 16:13:50 +0300

    comments here and there

--- http-server.c
+++ http-server.c
@@ -6,7 +6,7 @@
 
 int open_socket(){
   int sock_fd;
-
+  //SOCK_STREAM -> TCP/IP type of communication 
   sock_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (sock_fd < 0) {
     perror("error opening socket");
@@ -16,11 +16,15 @@ int open_socket(){
 }
 
 int bind_socket(int sock_fd, int port) {
+  
   struct sockaddr_in* sock_addr;
+  //AF_INET: address family of IPv4
   sock_addr->sin_family = AF_INET;
+  //htons/l convert from host byte order to network byte order
   sock_addr->sin_port = htons(port);
   sock_addr->sin_addr.s_addr = htonl(INADDR_ANY);
-
+  
+  //still a bit confused when it comes to the difference between sockaddr and sockaddr_in
   int bind_rc = bind(sock_fd, (struct sockaddr *)sock_addr, sizeof(sock_addr));
   if (bind_rc < 0) {
     perror("error binding socket");
@@ -32,7 +36,6 @@ int bind_socket(int sock_fd, int port) {
 }
 
 int main(int argc, char* argv[]){
-  printf("hello");
   int port = atoi(argv[1]);
   int sock_fd = open_socket();
   bind_socket(sock_fd, port);
