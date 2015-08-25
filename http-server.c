ee569555c028a403b8c99fee9fd29c1da1ad0c9e
Ruba Merza <ruba.merza@gmail.com>
2015-08-25 23:08:10 +0300

    Add more response lines

--- http-server.c
+++ http-server.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 //max number of bytes for words
-#define MAX_BYTES 100
-
+#define SIZE 100
 
 int parse_get_request(int sock_fd, char** body, int num_lines){
-  //TODO: deal with resource path
+  //split first line of request
   int rc;
-  char* words[MAX_BYTES];
-  
+  char* words[SIZE];
   char* tok = strtok(body[0], " ");
   int i = 0;
 
@@ -25,9 +25,10 @@ int parse_get_request(int sock_fd, char** body, int num_lines){
     words[i] = tok;
   }
 
-  char* path = malloc(MAX_BYTES);
+  //get the path from that line
+  char* path = malloc(SIZE);
 
-  if(strncmp(words[1], "/", MAX_BYTES) == 0) {
+  if(strncmp(words[1], "/", SIZE) == 0) {
     words[1] = "/Users/ruba/code/chttpd/index.html\0";
   } else {
     char abs_path[25] = "/Users/ruba/code/chttpd\0";
@@ -36,40 +37,46 @@ int parse_get_request(int sock_fd, char** body, int num_lines){
 
   strcat(path,  words[1]);
 
+  //check if resource exists
+  char* response = malloc(SIZE);
   if(access(path, F_OK) != -1) {
-    char* response = malloc(MAX_BYTES);
-    FILE* file;
     strncpy(response, words[2], strlen(words[2] + 1));
     strcat(response, " 200 OK\n\0");
+    //open file to extract lines 
+    FILE* file;
     file = fopen(path, "r");
-    char** text = malloc(MAX_BYTES);
+    char** text = malloc(SIZE);
     if(file) {
       char* line = NULL;
       size_t linecap = 0;
       ssize_t linelen = 0;
       while((linelen = getline(&line, &linecap, file)) > 0) {
-        *text = calloc(MAX_BYTES, MAX_BYTES);
-        strncpy(*text, line, MAX_BYTES);
+        *text = calloc(SIZE, SIZE);
+        strncpy(*text, line, SIZE);
         strcat(response, *text);
       }
     } else {
       perror("error opening file!");
     }
 
-    write(sock_fd, response, strlen(response));
     rc = 1;
   } else {
+    strncpy(response, words[2], strlen(words[2] + 1));
+    strcat(response, " 404 Not Found\n\0");
     printf("%s 404 File Not Found\n", words[2]);
     rc = 0;
   }
+
+  write(sock_fd, response, strlen(response));
   return rc;
 }
 
 int parse_head_request(int sock_fd, char** body, int num_lines) {
-  //TODO: figure out what to write back
-  char* words[MAX_BYTES];
+  //split the words of the first line of the request
+  char* words[SIZE];
   char* tok = strtok(body[0], " ");
   int i = 0;
+  int rc = 0;
 
   words[i] = tok;
   while(tok != NULL) {
@@ -78,35 +85,69 @@ int parse_head_request(int sock_fd, char** body, int num_lines) {
     words[i] = tok;
   }
 
-  char* path = malloc(MAX_BYTES);
-
-  if(strncmp(words[1], "/", MAX_BYTES) == 0) {
+  //store the path in relation to where chttpd lives
+  char* path = malloc(SIZE);
+  if(strncmp(words[1], "/", SIZE) == 0) {
     words[1] = "/Users/ruba/code/chttpd/index.html\0";
   } else {
     char abs_path[25] = "/Users/ruba/code/chttpd\0";
     strncpy(path, abs_path, strlen(abs_path) + 1);
   }
-
   strcat(path,  words[1]);
+  //write: DATE: content-type:text/html content-length 
+  char* response = malloc(SIZE);
 
   if(access(path, F_OK) != -1) {
-    char* response = malloc(MAX_BYTES);
-    FILE* file;
-    strncpy(response, words[2], strlen(words[2] + 1));
-    strcat(response, " 200 OK\n\0");
-    write(sock_fd, response, strlen(response));
+    //get current time and date
+    time_t current_time = time(NULL);
+    if(current_time == (time_t) - 1) {
+      fprintf(stderr, "error computing time\n");
+      return 0;
+    }
+    
+    struct tm* local_time = localtime(&current_time);
+    if(local_time == NULL) {
+      fprintf(stderr, "error converting time\n");
+      return 0;
+    }
+
+    char buff[SIZE];
+    strftime(buff, SIZE, "Date: %A, %d %B %Y %T %Z\n", local_time);
    
+    //get file information 
+    struct stat attribs;
+    int stat_rc = stat(path, &attribs);
+    if(stat_rc > 0) {
+      fprintf(stderr, "error getting file attribs\n");
+      return 0;
+    }
+
+    char date[SIZE];
+    struct tm* access_time = localtime(&(attribs.st_atime));
+    strftime(date, SIZE, "Last accessed:%A, %d %B %Y %T %Z\n", access_time);
+    
+    //print status line as 200 OK
+    strncpy(response, words[2], strlen(words[2] + 1));
+    strcat(response, " 200 OK\nDate: \0");
+    strcat(response, buff);
+    strcat(response, date);
+
+    rc = 1;
   } else {
-    printf("%s 404 File Not Found\n", words[2]);
+    strncpy(response, words[2], strlen(words[2] + 1));
+    strcat(response, " 404 Not Found\n\0");
+    printf("%s 404 Not Found\n", words[2]);
+    rc = 0;
   }
   
-  return 0;
+  write(sock_fd, response, strlen(response));
+  return rc;
 }
 
 int parse_request(char* buff, int sock_fd) {
   //TODO: fix the order of the read_from_client, parse_request.. functions
-  char* buff_copy = calloc(1, MAX_BYTES);
-  strncpy(buff_copy, buff, MAX_BYTES);
+  char* buff_copy = calloc(1, SIZE);
+  strncpy(buff_copy, buff, SIZE);
   int num_lines = 0;
   char* newline = strtok(buff_copy, "\n");
   while(newline != NULL) {
@@ -128,8 +169,8 @@ int parse_request(char* buff, int sock_fd) {
   //out the type(GET, POST, HEAD) of request
   char** str = calloc(num_lines, sizeof(char *));
   for (int x = 0; x < i; x++) {
-    str[x] = calloc(MAX_BYTES, sizeof(char));
-    strncpy(str[x], lines[x], MAX_BYTES * sizeof(char));
+    str[x] = calloc(SIZE, sizeof(char));
+    strncpy(str[x], lines[x], SIZE * sizeof(char));
   }
 
   //determine type of request from the first word of the message
